35eebe7ad63c81ed5a815dc2beac50b701644701
Maxendre Martin <maxendremartin@gmail.com>
2020-09-01 14:02:03 +0200

    correction dossier phpmailer

--- index.php
+++ index.php
@@ -3,9 +3,9 @@ use PHPMailer\PHPMailer\PHPMailer;
 use PHPMailer\PHPMailer\SMTP;
 use PHPMailer\PHPMailer\Exception;
 
-require 'phpmailer-master/src/PHPMailer.php';
-require 'phpmailer-master/src/SMTP.php';
-require 'phpmailer-master/src/Exception.php';
+require 'PHPMailer-master/src/PHPMailer.php';
+require 'PHPMailer-master/src/SMTP.php';
+require 'PHPMailer-master/src/Exception.php';
 
 $mail = new PHPMailer(true);
 
